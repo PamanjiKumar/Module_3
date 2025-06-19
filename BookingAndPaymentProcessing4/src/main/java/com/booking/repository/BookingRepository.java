d8ca20947881f7b51f575340233960545f0b7f21
PamanjiKumar <pamanji.kumar@cognizant.com>
2025-06-19 18:47:11 +0530

    Delete BookingAndPaymentProcessing4 directory

--- BookingAndPaymentProcessing4/src/main/java/com/booking/repository/BookingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.booking.repository;
-
-import com.booking.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
